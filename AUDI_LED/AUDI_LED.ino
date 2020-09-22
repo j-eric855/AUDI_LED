e9d95c666e5f29adbbab27506317d43845bd6be8
j-eric855 <71041220+j-eric855@users.noreply.github.com>
2020-09-22 14:04:04 +0200

    zweidimensionales Array

--- AUDI_LED/AUDI_LED.ino
+++ AUDI_LED/AUDI_LED.ino
@@ -6,7 +6,15 @@
    Hardware:        Pin 2-5 rote LEDs im Laufmuster
                     Pin 6-9 grüne LEDs im Laufmzuster
 */
-byte LED[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
+byte ledZustand[6][8] = {
+  {1, 0, 0, 0, 0, 0, 0, 1},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 0, 0, 1, 1, 0, 0, 0},
+  {0, 0, 1, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 1, 0},
+};
 
 #define Laufzeit 100  // Verzögerung in ms zwischen den LEDs wird eingestellt
 
@@ -15,23 +23,18 @@ void setup()
   /******Pinbelegung********/
   for (byte i = 0; i < 8; i++)
   {
-    pinMode(LED[i], OUTPUT);
+    pinMode(led[i], OUTPUT);
   }
 }
 
 void loop()
 {
-  for (byte i = 0; i < 8; i++)
+  for (byte i = 0; i < 6; i++)      //Zeilen
   {
-    digitalWrite(LED[i], HIGH);
-    delay(Laufzeit);
-    digitalWrite(LED[i], LOW);
-  }
-
-  for (byte i = 6; i > 0; i--)
-  {
-    digitalWrite(LED[i], HIGH);
-    delay(Laufzeit);
-    digitalWrite(LED[i], LOW);
+    for (byte j = 0; j < 8; j++)    //Spalten
+    {
+      digitalWrite(led[j], ledZustand[i][j]);
+    }
+    delay(150);
   }
 }
